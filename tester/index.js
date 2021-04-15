65b290c58847082f04e672ea3cb906023ab79331
DrakeGoCoding <hoanglongphi21@gmail.com>
2021-04-15 20:45:20 +0700

    nah

---
+++ tester/index.js
@@ -0,0 +1,13 @@
+const add = (a, b) =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a !== 'number' || typeof b !== 'number') reject("Datatype Error")
+            else resolve(a + b);
+        }, 1000);
+    })
+
+
+add(3, 8).then(
+    result => console.log(result),
+    error => console.log(error)
+);
